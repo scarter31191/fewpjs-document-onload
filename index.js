cefa1c8f3a7af562732c3c752cdfc1ac443607db
scarter31191 <scarter31191@gmaile.com>
2021-03-07 07:51:46 -0500

    commit tree 5cdb73be5b7b6c192ed48ac1ef94783fd0cbcbf9

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    newDOM(); // call on the function to when the DOMContentLoaded is triggered
+  }); 
+
+   function newDOM() {
+       document.getElementById("text")
+        text.innerHTML = "This is really cool!"; // .innerHTML works too since getElementById already grabbed it.
+   }
